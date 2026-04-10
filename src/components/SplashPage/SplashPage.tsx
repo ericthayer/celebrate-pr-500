2fcbf6f4ac2c4e555bdc1d55e16823a8a94ce534
Eric Thayer <eric.thayer594@gmail.com>
2026-04-10 10:44:42 -0600

    Updated SplashPage.tsx

--- src/components/SplashPage/SplashPage.tsx
+++ src/components/SplashPage/SplashPage.tsx
@@ -370,7 +370,7 @@ export default function SplashPage() {
             variant="outlined"
             color="primary"
             size="large"
-            startIcon={<StarIcon />}
+            startIcon={<CelebrateIcon />}
             sx={{
               px: 4,
               py: 1.5,
