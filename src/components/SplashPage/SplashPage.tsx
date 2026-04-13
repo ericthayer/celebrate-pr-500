0931344609bb5f56df6016a05646f52e7d271845
Eric Thayer <eric.thayer594@gmail.com>
2026-04-13 07:59:18 -0600

    Updated SplashPage.tsx

--- src/components/SplashPage/SplashPage.tsx
+++ src/components/SplashPage/SplashPage.tsx
@@ -11,7 +11,6 @@ import MergeIcon from '@mui/icons-material/MergeType';
 import PauseIcon from '@mui/icons-material/PauseCircleOutline';
 import PlayIcon from '@mui/icons-material/PlayCircleOutline';
 import CodeIcon from '@mui/icons-material/Code';
-import StarIcon from '@mui/icons-material/Star';
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import CelebrationIcon from '@mui/icons-material/Celebration';
