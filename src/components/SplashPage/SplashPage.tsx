6c4842a26796bd0753e1a8d8fd3a1e5e033582a3
Eric Thayer <eric.thayer594@gmail.com>
2026-04-13 08:00:01 -0600

    Updated SplashPage.tsx

--- src/components/SplashPage/SplashPage.tsx
+++ src/components/SplashPage/SplashPage.tsx
@@ -352,6 +352,7 @@ export default function SplashPage() {
             rel="noopener noreferrer"
             component="a"
             sx={{
+              flex: '1 1 12rem',
               px: 4,
               py: 1.5,
               fontSize: '0.9rem',
@@ -381,6 +382,7 @@ export default function SplashPage() {
             rel="noopener noreferrer"
             component="a"
             sx={{
+              flex: '1 1 12rem',
               px: 4,
               py: 1.5,
               fontSize: '0.9rem',
